cd663ff8d532f9604915f180f4e5c3e503d421c2
Gabriel Macena <gabriel.macena.bm@gmail.com>
2025-12-26 14:10:28 -0300

    feat: add manual destroy trigger, helm controller and final ci/cd logic

---
+++ modules/eks/helm_lb_controller.tf
@@ -0,0 +1,28 @@
+resource "helm_release" "lb_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+
+  set {
+    name  = "clusterName"
+    value = aws_eks_cluster.this.name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.aws_lb_controller.arn
+  }
+
+  depends_on = [aws_eks_node_group.this]
+}
